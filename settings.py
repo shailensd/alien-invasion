81542671ff0ec62c18c5ff970d7608f984ca165f
shailensd <155351004+shailensd@users.noreply.github.com>
2025-06-19 13:52:35 -0700

    Run the game in full screen mode(also, change the screen height back to 800)

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
         """Initialize the game settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 750
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         
         # Ship settings
