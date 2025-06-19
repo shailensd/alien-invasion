06b710332a5283c38426062136dc43aa8dba46bb
shailensd <155351004+shailensd@users.noreply.github.com>
2025-06-19 10:45:17 -0700

    Change screen height to 750

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ class Settings:
         """Initialize the game settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 750
         self.bg_color = (230, 230, 230)
