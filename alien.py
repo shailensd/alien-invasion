4cd0ac7b93c567281f2bd800e2ace889a6a26259
shailensd <155351004+shailensd@users.noreply.github.com>
2025-07-02 14:47:01 -0700

    Fix the code in alien.py

--- alien.py
+++ alien.py
@@ -5,7 +5,7 @@ class Alien(Sprite):
     """A class to represent a single alien in the fleet"""
     def __init__(self, ai_game):
         """Initialize the alien and set its starting position"""
-        super.__init__()
+        super().__init__()
         self.screen = ai_game.screen
         
         # Load the alien image and set rect attribute
