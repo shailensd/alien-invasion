ff2cf3192ff5304c1cdf9374dfbf8373f5fcf5ab
shailensd <155351004+shailensd@users.noreply.github.com>
2025-07-08 17:12:16 -0700

    Stop the game after certain number of times(Game Over)

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,14 +28,18 @@ class AlienInvasion:
         
         self._create_fleet()
         
+        # Start Alien Invasion in an active state.
+        self.game_active = True
+        
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
             
@@ -146,20 +150,23 @@ class AlienInvasion:
         
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
-        # Decrement ships left.
-        self.stats.ship_left -=1
+        if self.stats.ship_left > 0:
+            # Decrement ships left.
+            self.stats.ship_left -=1
         
-        # Get rid of any remaining bullets and aliens.
-        self.bullets.empty()
-        self.aliens.empty()
+            # Get rid of any remaining bullets and aliens.
+            self.bullets.empty()
+            self.aliens.empty()
         
-        # Create a new fleet and center the ship.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
         
-        # Pause
-        sleep(0.5)
-        
+            # Pause
+            sleep(0.5)
+        else: 
+            self.game_active = False
+            
     def _check_alien_bottom(self):
         """Check if any aliens have reached the bottom of the screen"""
         for alien in self.aliens.sprites():
