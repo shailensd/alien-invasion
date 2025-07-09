61092db47f1abf035680c369849cbf16ad6f8dee
shailensd <155351004+shailensd@users.noreply.github.com>
2025-07-09 15:03:21 -0700

    Modify the speed settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -68,6 +68,7 @@ class AlienInvasion:
             # Destroy exiting bullets and create a new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
     
     def _update_aliens(self):
         """Check if the fleet is at an adge and Update the positions of 
