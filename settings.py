13b870a14d11cfc328d10adc8b95f007ffc37c2b
shailensd <155351004+shailensd@users.noreply.github.com>
2025-07-12 12:02:14 -0700

    Increase point value as difficulty increases

--- settings.py
+++ settings.py
@@ -23,6 +23,8 @@ class Settings:
         #How quickly the game speeds up
         self.speedup_scale = 1.1
         self.initialize_dynamic_settings()
+        # How quickly the alien point value increase
+        self.score_scale = 1.5
         
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
@@ -40,3 +42,5 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+    
